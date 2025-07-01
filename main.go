7d9076d42841114511ace6511385ec464846e87c
vicpoo <157589559+vicpoo@users.noreply.github.com>
2025-07-01 16:11:07 -0600

    .env y coneccion exitosa a la BD

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
+// main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/vicpoo/apigestion-solar-go/src/core" 
+)
 
 func main() {
-	fmt.Println("Hola Edwin, esta es la API Gestional")
+
+	// Inicializar la base de datos
+	core.InitDB()
+
+	// Verifica si la conexión sigue viva
+	db := core.GetBD()
+	err := db.Ping()
+	if err != nil {
+		log.Fatal("No se pudo hacer ping a la base de datos:", err)
+	}
+
+	fmt.Println("¡Conexión a la base de datos verificada exitosamente!")
 }
